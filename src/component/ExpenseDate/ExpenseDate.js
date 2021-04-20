04cc20517d4a5411976b384ceb6979f35d54a824
Turhan <turhansel@gmail.com>
2021-04-20 18:10:31 +0300

    Initial commit

---
+++ src/component/ExpenseDate/ExpenseDate.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ExpenseDate.scss";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div className="month">{month}</div>
+      <div className="year">{year}</div>
+      <div className="day">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
---
+++ src/component/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
+import "./ExpenseItem.scss";
+import Card from "../Card/Card";
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="description">
+        <h2>{props.title}</h2>
+        <div className="price">₺{props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
