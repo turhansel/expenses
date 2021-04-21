06c614165716c01b7f43293e9a8ef0ae6763e5cc
Turhan <turhansel@gmail.com>
2021-04-21 14:53:43 +0300

    add ExpensesFilter and update Expenses.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
+import React, { useState } from "react";
 import Expenses from "./component/Expenses/Expenses";
-import React from "react";
 import NewExpense from "./component/NewExpense/NewExpense";
+
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
--- src/component/ExpenseItem/ExpenseItem.js
+++ src/component/ExpenseItem/ExpenseItem.js
@@ -1,19 +1,13 @@
-import React, { useState } from "react";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import "./ExpenseItem.scss";
 import Card from "../Card/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="price">{props.amount} 💸 </div>
       </div>
     </Card>
